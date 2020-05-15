21ad897c91390437cc9ab954f77981a1ce7fa27a
JDMTias <matiasiturbide@gmail.com>
2020-05-15 12:27:40 -0600

    All done except styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Character = props => {
-    return (
+const Character = (props) => {
+  return (
+    <div>
+      {props.characterData.map((character) => (
         <div>
-            {props.characterData.map( character =>
-                <div>
-                    <h2>{character.name}</h2>
-                    <p>Birthyear: {character.birth_year}</p>
-                    <p>Homeworld: {character.homworld}</p>
-                    <p>Films: {character.films}</p>
-                    <p></p>
-
-
-                </div>)}
-
+          <h2>{character.name}</h2>
+          <p>Birthyear: {character.birth_year}</p>
+          <p>Homeworld: {character.homworld}</p>
+          <p>Height: {character.height}</p>
+          <p>Hair Color: {character.hair_color}</p>
+          <p>Eye Color: {character.eye_color}</p>
+          <p>Gender: {character.gender}</p>
         </div>
-    )
+      ))}
+    </div>
+  );
+};
 
-}
-
-export default Character
+export default Character;
