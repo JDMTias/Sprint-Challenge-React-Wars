24239d12ca9c965e85b97ce3626e101877a18395
JDMTias <matiasiturbide@gmail.com>
2020-06-12 13:47:43 -0600

    all done

---
+++ starwars/src/components/GlobalStyles.jsx
@@ -0,0 +1,12 @@
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+   @import url('https://fonts.googleapis.com/css2?family=Megrim&display=swap');
+
+    body {
+        font-family: 'Megrim', cursive;
+        background-size: fill;
+    }
+    
+`
+export default GlobalStyles
